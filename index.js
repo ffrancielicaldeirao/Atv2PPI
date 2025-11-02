f7c2a248c1b14aab61c1bf0787410d93d622f898
ffrancielicaldeirao <francieli.aparecida@homail.com>
2025-11-02 20:10:36 -0300

    Adicionando arquivo vercel.

--- index.js
+++ index.js
@@ -260,6 +260,7 @@ server.get("/listarClientes", (requisicao, resposta) => {
         </div>
         <a class="btn btn-secondary" href="/cadastrarCliente">Voltar</a>
     </body>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     </html>
     `;
     resposta.send(tabelaClientes);
