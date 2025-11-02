6591d46b82c9929d547bc4e79016a9291346ec1c
ffrancielicaldeirao <francieli.aparecida@homail.com>
2025-11-02 19:27:07 -0300

    Finalização da implementação do metodo Post e a listagem dos clientes cadastrados.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var listaClientes = [];
 
 const server = express();
 
+server.use(express.urlencoded({ extended: true }));
+
 server.get("/", (requisicao, resposta) => {
     //disponibiliza o menu
     resposta.send(`
@@ -56,13 +58,13 @@ server.get("/CadastrarClientes", (requisicao, resposta) => {
           <form method="POST" action="/adicionarCliente" class="row g-3 needs-validation" novalidate>
           <div class="col-md-4">
             <label for="nome" class="form-label">Nome</label>
-            <input type="text" class="form-control" id="nome" placeholder="Francieli" required>
+            <input type="text" class="form-control" id="nome" name="nome" placeholder="Francieli" required>
             <div class="valid-feedback">Muito bom!</div>
           </div>
 
           <div class="col-md-4">
             <label for="sobrenome" class="form-label">Sobrenome</label>
-            <input type="text" class="form-control" id="sobrenome" placeholder="Caldeirao" required>
+            <input type="text" class="form-control" id="sobrenome" name="sobrenome" placeholder="Caldeirao" required>
             <div class="valid-feedback">Muito bom!</div>
           </div>
 
@@ -70,56 +72,56 @@ server.get("/CadastrarClientes", (requisicao, resposta) => {
             <label for="nomeUsuario" class="form-label">Nome de Usuário</label>
             <div class="input-group has-validation">
               <span class="input-group-text" id="inputGroupPrepend">@</span>
-              <input type="text" class="form-control" id="nomeUsuario" aria-describedby="inputGroupPrepend" required>
+              <input type="text" class="form-control" id="nomeUsuario" name="nomeUsuario" aria-describedby="inputGroupPrepend" required>
               <div class="invalid-feedback">Por favor, escolha um nome de usuário.</div>
             </div>
           </div>
 
           <div class="col-md-6">
             <label for="validacaoEmail" class="form-label">Email</label>
-            <input type="email" class="form-control" id="validacaoEmail" placeholder="exemplo@email.com" required>
+            <input type="email" class="form-control" id="validacaoEmail" name="email" placeholder="exemplo@email.com" required>
             <div class="invalid-feedback">Por favor, forneça um email válido.</div>
           </div>
 
             <div class="col-md-6">
               <label for="validacaoTelefone" class="form-label">Telefone</label>
-              <input type="tel" class="form-control" id="validacaoTelefone" placeholder="(00) 00000-0000" required>
+              <input type="tel" class="form-control" id="validacaoTelefone" name="telefone" placeholder="(00) 00000-0000" required>
               <div class="invalid-feedback">Por favor, forneça um número de telefone válido.</div>
             </div>
 
             <div class="col-md-6">
               <label for="validacaoSenha" class="form-label">Senha</label>
-              <input type="password" class="form-control" id="validacaoSenha" required>
+              <input type="password" class="form-control" id="validacaoSenha" name="senha" required>
               <div class="invalid-feedback">Por favor, forneça uma senha.</div>
             </div>
 
             <div class="col-md-6">
               <label for="validacaoConfirmSenha" class="form-label">Confirmar Senha</label>
-              <input type="password" class="form-control" id="validacaoConfirmSenha" required>
+              <input type="password" class="form-control" id="validacaoConfirmSenha" name="confirmarSenha" required>
               <div class="invalid-feedback">As senhas não coincidem.</div>
             </div>
 
             <div class="col-md-4">
               <label for="validacaoNascimento" class="form-label">Data de Nascimento</label>
-              <input type="date" class="form-control" id="validacaoNascimento" required>
+              <input type="date" class="form-control" id="validacaoNascimento" name="nascimento" required>
               <div class="invalid-feedback">Por favor, forneça sua data de nascimento.</div>
             </div>
 
             <div class="col-md-8">
               <label for="validacaoEndereco" class="form-label">Endereço</label>
-              <input type="text" class="form-control" id="validacaoEndereco" placeholder="Rua, número, bairro" required>
+              <input type="text" class="form-control" id="validacaoEndereco" name="endereco" placeholder="Rua, número, bairro" required>
               <div class="invalid-feedback">Por favor, forneça seu endereço.</div>
             </div>
 
             <div class="col-md-6">
               <label for="validacaoCidade" class="form-label">Cidade</label>
-              <input type="text" class="form-control" id="validacaoCidade" required>
+              <input type="text" class="form-control" id="validacaoCidade" name="cidade" required>
               <div class="invalid-feedback">Por favor, forneça uma cidade válida.</div>
             </div>
 
             <div class="col-md-3">
               <label for="validacaoEstado" class="form-label">Estado</label>
-              <select class="form-select" id="validacaoEstado" required>
+              <select class="form-select" id="validacaoEstado" name="estado" required>
                 <option selected disabled value="">Escolha...</option>
                 <option>SP</option>
                 <option>RJ</option>
@@ -131,7 +133,7 @@ server.get("/CadastrarClientes", (requisicao, resposta) => {
 
             <div class="col-md-3">
               <label for="validacaoCep" class="form-label">CEP</label>
-              <input type="text" class="form-control" id="validacaoCep" placeholder="00000-000" required>
+              <input type="text" class="form-control" id="validacaoCep" name="cep" placeholder="00000-000" required>
               <div class="invalid-feedback">Por favor, forneça um CEP válido.</div>
             </div>
 
@@ -201,8 +203,66 @@ server.get("/CadastrarClientes", (requisicao, resposta) => {
 `);     
 });
 server.post("/adicionarCliente", (requisicao, resposta) => {
-    console.log("Cliente adicionado com sucesso!");
-    listaClientes.push();
+    const name = requisicao.body.nome;
+    const sobrenome = requisicao.body.sobrenome;
+    const telefone = requisicao.body.telefone;
+    const email = requisicao.body.email;
+    const cidade = requisicao.body.cidade;
+    const estado = requisicao.body.estado;
+    const cep = requisicao.body.cep;
+
+    listaClientes.push({name, sobrenome, telefone, email, cidade, estado, cep});
+    resposta.redirect("/listarClientes");
+});
+server.get("/listarClientes", (requisicao, resposta) => {
+    let tabelaClientes = `
+    <DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/x-icon" href="/static/favicon.ico">
+        <link rel="stylesheet" href="/static/bootstrap.min.css">
+        <title>Lista de Clientes</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Lista de Clientes</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Sobrenome</th>
+                        <th>Telefone</th>
+                        <th>Email</th>
+                        <th>Cidade</th>
+                        <th>Estado</th>
+                        <th>CEP</th>
+                    </tr>
+                </thead>
+                <tbody>`;
+                   for(let i=0; i < listaClientes.length; i++) {
+                       tabelaClientes += `
+                           <tr>
+                               <td>${listaClientes[i].name}</td>
+                               <td>${listaClientes[i].sobrenome}</td>
+                               <td>${listaClientes[i].telefone}</td>
+                               <td>${listaClientes[i].email}</td>
+                               <td>${listaClientes[i].cidade}</td>
+                               <td>${listaClientes[i].estado}</td>
+                               <td>${listaClientes[i].cep}</td>
+                           </tr>
+                       `;
+                   }
+    tabelaClientes += `   
+                </tbody>
+            </table>
+        </div>
+        <a class="btn btn-secondary" href="/cadastrarCliente">Voltar</a>
+    </body>
+    </html>
+    `;
+    resposta.send(tabelaClientes);
 });
 
 server.listen(porta, host, () => {
